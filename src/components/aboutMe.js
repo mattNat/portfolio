6be8b9183692bf97632c1231d00e5a81e0825f29
mattNat <mattsanpedro76@gmail.com>
2018-05-17 23:18:41 -0700

    finish about me

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import ProfilePicture from '../img/IMG_5975.JPG';
 import '../style/aboutMe.css';
 
 export default function AboutMe() {
   return (
-    <div className='row section-intro about-me'>
-      <div className='col-twelve'>
+    <div className='row about-me'>
+      <div className='col-twelve section-intro'>
 
         <h1>About Me</h1>
         <h3>Let me introduce myself.</h3>
 
         <div class="intro-info">
 
-          <img src={require('../img/IMG_5975.JPG')} alt="Profile Picture" width='100px'/>
+          <img src={ProfilePicture} alt="Profile Picture" width='150px'/>
 
-          <p class="lead">Full stack developer with a strong background in mathematics and computational science. 
+          <p class="lead">Full stack developer with a strong background math. 
           Proficient in mobile-first responsive design, applying principles of test-driven-development to React 
           JavaScript ES5/ES6 framework. Extensive knowledge in data modelling from advanced engineering degree 
           and internship experience. Continuing education in Thinkful Engineering Immersion program with 
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import '../style/aboutMe.css';
+import '../style/projects.css';
 
 export default function Projects() {
   return (
@@ -7,9 +7,6 @@ export default function Projects() {
       <div className='col-twelve'>
         <h1>Projects</h1>
         <div class="intro-info">
-
-          <img src={require('../img/IMG_5975.JPG')} alt="Profile Picture" width='100px'/>
-
           <p class="lead">Full stack developer with a strong background in mathematics and computational science. 
           Proficient in mobile-first responsive design, applying principles of test-driven-development to React 
           JavaScript ES5/ES6 framework. Extensive knowledge in data modelling from advanced engineering degree 
